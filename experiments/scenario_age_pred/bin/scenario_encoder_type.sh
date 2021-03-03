3330ca371c1de7819df83de73fafb89c50ef7cd2
Darkhan <nur.darhan@gmail.com>
2021-03-03 09:18:32 +0000

    "first try of rewriting modules configs to pytorch lightning"

--- experiments/scenario_age_pred/bin/scenario_encoder_type.sh
+++ experiments/scenario_age_pred/bin/scenario_encoder_type.sh
@@ -1,42 +1,44 @@
 # LSTM encoder
 export SC_SUFFIX="encoder_lstm"
-python ../../metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.rnn.type="lstm" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
-python ../../ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    output.path="data/emb__$SC_SUFFIX" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
+python ../../pl_train_module.py \
+  logger_name=${SC_SUFFIX} \
+  params.rnn.type="lstm" \
+  model_path="models/age_pred_mlm__$SC_SUFFIX.p" \
+  --conf conf/mles_params.hocon
+
+python ../../pl_inference.py \
+  model_path="models/age_pred_mlm__$SC_SUFFIX.p" \
+  output.path="data/emb__$SC_SUFFIX" \
+  --conf conf/mles_params.hocon
 
 # Transformer encoder
 export SC_SUFFIX="encoder_transf"
-python ../../metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.batch_size=128 \
-    params.transf.train_starter=true \
-    params.transf.dropout=0.1 \
-    params.transf.max_seq_len=800 \
-    params.transf.n_heads=4 \
-    params.transf.input_size=128 \
-    params.transf.dim_hidden=128 \
-    params.transf.n_layers=4 \
-    params.transf.shared_layers=false \
-    params.transf.use_after_mask=false \
-    params.transf.use_positional_encoding=false \
-    params.transf.use_src_key_padding_mask=false \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
-python ../../ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="data/emb__$SC_SUFFIX" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
+python ../../pl_train_module.py \
+  logger_name=${SC_SUFFIX} \
+  params.encoder_type="transf" \
+  params.train.batch_size=128 \
+  params.transf.train_starter=true \
+  params.transf.dropout=0.1 \
+  params.transf.max_seq_len=800 \
+  params.transf.n_heads=4 \
+  params.transf.input_size=128 \
+  params.transf.dim_hidden=128 \
+  params.transf.n_layers=4 \
+  params.transf.shared_layers=false \
+  params.transf.use_after_mask=false \
+  params.transf.use_positional_encoding=false \
+  params.transf.use_src_key_padding_mask=false \
+  model_path="models/age_pred_mlm__$SC_SUFFIX.p" \
+  --conf conf/mles_params.hocon
 
+python ../../pl_inference.py \
+  model_path="models/age_pred_mlm__$SC_SUFFIX.p" \
+  inference_dataloader.loader.batch_size=128 \
+  output.path="data/emb__$SC_SUFFIX" \
+  --conf conf/mles_params.hocon
+
+# Compare
+rm results/scenario_age_pred__encoder_types.csv
 
 # Compare
 python -m scenario_age_pred compare_approaches --output_file "results/scenario_age_pred__encoder_types.csv" \
--- experiments/scenario_age_pred/bin/scenario_sub_seq_sampling_strategy.sh
+++ experiments/scenario_age_pred/bin/scenario_sub_seq_sampling_strategy.sh
@@ -1,35 +1,33 @@
 export SC_SUFFIX="SampleRandom"
 export SC_STRATEGY="SampleRandom"
-python ../../metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.train.split_strategy.split_strategy=$SC_STRATEGY \
-    params.valid.split_strategy.split_strategy=$SC_STRATEGY \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
+python ../../pl_train_module.py \
+    logger_name=${SC_SUFFIX} \
+    data_module.train.split_strategy.split_strategy=$SC_STRATEGY \
+    data_module.valid.split_strategy.split_strategy=$SC_STRATEGY \
+    model_path="models/age_pred_mlm__$SC_SUFFIX.p" \
     params.train.split_strategy.cnt_min=200 \
     params.train.split_strategy.cnt_max=600 \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
-python ../../ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
+    --conf "conf/mles_params.hocon"
+python ../../pl_inference.py \
+    model_path="models/age_pred_mlm__$SC_SUFFIX.p" \
     output.path="data/emb__$SC_SUFFIX" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
+    --conf "conf/mles_params.hocon"
 
 
 export SC_SUFFIX="SplitRandom"
 export SC_STRATEGY="SplitRandom"
-python ../../metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.train.split_strategy.split_strategy=$SC_STRATEGY \
-    params.valid.split_strategy.split_strategy=$SC_STRATEGY \
-    params.train.max_seq_len=600 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
-python ../../ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
+python ../../pl_train_module.py \
+    logger_name=${SC_SUFFIX} \
+    data_module.train.max_seq_len=600 \
+    data_module.valid.max_seq_len=600 \
+    data_module.train.split_strategy.split_strategy=$SC_STRATEGY \
+    data_module.valid.split_strategy.split_strategy=$SC_STRATEGY \
+    model_path="models/age_pred_mlm__$SC_SUFFIX.p" \
+    --conf "conf/mles_params.hocon"
+python ../../pl_inference.py \
+    model_path="models/age_pred_mlm__$SC_SUFFIX.p" \
     output.path="data/emb__$SC_SUFFIX" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
-
+    --conf "conf/mles_params.hocon"
 
 # Compare
 python -m scenario_age_pred compare_approaches --output_file "results/scenario_age_pred__subseq_smpl_strategy.csv" \
